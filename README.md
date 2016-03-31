92adeb4eb556a2142cbc1e0fce2ef5474676244c
Teffen Ellis (Eric) <teffen@nirri.us>
2016-03-31 04:04:59 -0700

    Flesh out boilerplate.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Clippy.js
+
+Add Clippy or his friends to any website for instant nostalgia.
+Our research shows that people love two things: failed Microsoft technologies and obscure Javascript libraries.
+Naturally, we decided to combine the two.
---
+++ build/app.js
@@ -0,0 +1,28 @@
+(function () {
+  // Check for IE9+
+  if (!window.addEventListener) return;
+
+  let options = INSTALL_OPTIONS;
+  let agent;
+
+  function updateElement() {
+    clippy.load(options.agent, nextAgent => {
+      agent = nextAgent;
+      agent.show();
+    }, () => null, "//s3.amazonaws.com/clippy.js/Agents/");
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", updateElement);
+  } else {
+    updateElement();
+  }
+
+  INSTALL_SCOPE = { // eslint-disable-line no-undef
+    setOptions(nextOptions) {
+      options = nextOptions;
+
+      agent.hide(true, updateElement);
+    }
+  };
+})();
